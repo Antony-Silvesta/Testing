f3141d2ab8a664934e9d1f19a7359709e9b17aab
Antony-Silvesta <antonysilvesta74@gmail.com>
2024-10-20 12:29:53 +0530

    Add Selenium tests and CI/CD configuration

---
+++ test_google.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+def test_google_search():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")  # Run in headless mode
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    # Connect to the remote Selenium instance
+    driver = webdriver.Remote(
+        command_executor="http://localhost:4444/wd/hub",
+        options=options
+    )
+    driver.get("https://www.google.com")
+    assert "Google" in driver.title
+    driver.quit()
